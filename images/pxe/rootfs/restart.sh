eef06b266ede29d3bc1f20d366b8f7a3ead4fccf
fire833 <kttpsy@gmail.com>
2023-09-12 00:04:34 -0500

    feat(pxe): more dinking with trying to get this to work

---
+++ images/pxe/rootfs/restart.sh
@@ -0,0 +1,3 @@
+
+pkill dnsmasq
+dnsmasq
--- scripts/runImage.sh
+++ scripts/runImage.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-podman run -it --privileged -v fai:/srv/fai/config -v $1:/srv/tftp/fai -p 69:69 localhost/fai-installer:latest
+# podman run -it --privileged -v fai:/srv/fai/config -v $1:/srv/tftp/fai -p 69:69 localhost/fai-installer:latest
+podman run --cap-add=net_admin,net_raw --net=host -it $1 bash
